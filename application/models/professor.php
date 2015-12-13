a2f517dbd5ac878fc78eb9536ff8916f16abe1d5
iamcheche <chebalane@gmail.com>
2015-12-13 11:43:54 -0800

    first push of the website

---
+++ application/models/professor.php
@@ -0,0 +1,20 @@
+<?php
+    Class Admin extends CI_Model{
+
+        function login($username, $password){
+            $this -> db -> select('*');
+            $this -> db -> from('professors');
+            $this -> db -> where('professor_username', $username);
+            $this -> db -> where('profesor_password', $password);
+            $this -> db -> limit(1);
+
+            $query = $this -> db -> get();
+
+            if($query -> num_rows() == 1){
+                return $query->result();
+            }else{
+                return false;
+            }
+        }
+    }
+?>
---
+++ application/models/professor_model.php
@@ -0,0 +1,10 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+	class Professor_model extends CI_Model {
+
+		public function __construct(){
+			$this->load->database();
+			$this->load->library('session');
+		}
+		
+	}
+?>
